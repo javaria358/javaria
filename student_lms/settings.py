c2f7c4daaf06c1031e01e088eef7931f5b8d8196
javaria358 <javariaasgharch358@gmail.com>
2026-07-09 13:50:09 +0500

    settings.py

--- student_lms/settings.py
+++ student_lms/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ldo!zg!fj3)sm+0kdwdwkt)g%1-mvj^dxhnvp%yp-=rk7#vbmf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app", "127.0.0.1", "localhost"]
